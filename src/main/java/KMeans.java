c7e9a396129ae6d8e8911fcfe8169cc8858e6f06
Muhammad Magdy <muh.magdy1@gmail.com>
2024-03-16 12:04:20 +0200

    Added iterations

--- src/main/java/KMeans.java
+++ src/main/java/KMeans.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -78,16 +77,26 @@ public class KMeans {
         }
         return centroids;
     }
+    
+    private static boolean hasConverged(Point[] oldCentroids, Point[] newCentroids, float threshold) {
+        for (int i = 0; i < oldCentroids.length; i++) {
+            if (oldCentroids[i].distance(newCentroids[i]) > threshold) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) throws Exception {
         if (args.length != 4) {
-            System.err.println("Usage: KMeans <input path> <output path> <k> <dimensions>");
+            System.err.println("Usage: KMeans <input path> <output path> <k> <dimensions> <max iterations>");
             System.exit(-1);
         }
         
         Configuration conf = new Configuration();
         int k = Integer.parseInt(args[2]);
         int dimensions = Integer.parseInt(args[3]);
+        int maxIterations = Integer.parseInt(args[4]);
         conf.set("k", args[2]);
         conf.set("dimensions", args[3]);
         
@@ -95,18 +104,51 @@ public class KMeans {
         for (int i = 0; i < k; i++) {
             conf.set("c" + i, centroids[i].toString());
         }
+        
+        boolean converged = false;
+        int iteration = 0;
+        while(!converged){
+            Job job = Job.getInstance(conf, "KMeans Iteration " + iteration);
+            job.setJarByClass(KMeans.class);
+            job.setMapperClass(KMeansMapper.class);
+            job.setCombinerClass(KMeansCombiner.class);
+            job.setReducerClass(KMeansReducer.class);
+            job.setOutputKeyClass(IntWritable.class);
+            job.setOutputValueClass(Point.class);
+            job.setMapOutputKeyClass(IntWritable.class);
+            job.setMapOutputValueClass(Point.class);
+            boolean success = job.waitForCompletion(true);
+            
+            if(!success){
+                System.err.println("Error running iteration " + iteration);
+                System.exit(1);
+            }
+            
+            if (iteration >= maxIterations) {
+                System.out.println("Max iterations reached");
+                break;
+            }
+            
+            Point[] oldCentroids = centroids;
+            centroids = new Point[k];
+            for (int i = 0; i < k; i++) {
+                centroids[i] = new Point(conf.get("c" + i));
+            }
 
-        Job job = Job.getInstance(conf, "KMeans");
-        job.setJarByClass(KMeans.class);
-        job.setMapperClass(KMeansMapper.class);
-        job.setCombinerClass(KMeansCombiner.class);
-        job.setReducerClass(KMeansReducer.class);
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(Point.class);
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(Point.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+            converged = hasConverged(oldCentroids, centroids, 1e-6f);
+
+            if(converged){
+                System.out.println("Converged after " + iteration + " iterations");
+                FileInputFormat.addInputPath(job, new Path(args[0]));
+                FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            }
+            else{
+                for (int i = 0; i < k; i++) {
+                    conf.unset("c" + i);
+                    conf.set("c" + i, centroids[i].toString());
+                }
+            }
+            iteration++;
+        }        
     }
 }
